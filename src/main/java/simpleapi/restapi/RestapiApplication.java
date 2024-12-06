173b0d1182598f72720c196eeb725e007761e4d8
Inigojeevan <in1442@srmist.edu.in>
2024-12-06 23:12:40 +0530

    feat: binded application context to server

--- src/main/java/simpleapi/restapi/RestapiApplication.java
+++ src/main/java/simpleapi/restapi/RestapiApplication.java
@@ -2,12 +2,17 @@ package simpleapi.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestapiApplication.class);
+	}
 }
