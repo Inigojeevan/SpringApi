c09cabc6b07ec4ea1f73118ba12e9a8721da2dc4
Inigojeevan <in1442@srmist.edu.in>
2024-12-08 20:09:08 +0530

    feat: removed servletInitializer

--- src/main/java/simpleapi/restapi/RestapiApplication.java
+++ src/main/java/simpleapi/restapi/RestapiApplication.java
@@ -2,17 +2,10 @@ package simpleapi.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestapiApplication extends SpringBootServletInitializer {
-
+public class RestapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
-	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(RestapiApplication.class);
 	}
 }
